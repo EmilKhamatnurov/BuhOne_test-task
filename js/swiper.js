876e2cb9f799f7a32ed643b043cdb9b069e66538
emil-khamatnurov <eh-lens@yandex.ru>
2023-02-11 15:08:35 +0500

    Свайпер clients_Swiper
    Добавлен второй свайпер, но появилась проблема с переключением слайдов во стором свайпере

--- js/swiper.js
+++ js/swiper.js
@@ -13,3 +13,18 @@ new Swiper('.swiper', {
 	  prevEl: '.swiper-button-prev',
 	}
   });
+  new Swiper('.clients_swiper', {
+	// Optional parameters
+	loop: true,
+  
+	// If we need pagination
+	pagination: {
+	  el: '.clients-swiper_pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.clients-swiper-button-next',
+	  prevEl: '.clients-swiper-button-prev',
+	}
+  });
